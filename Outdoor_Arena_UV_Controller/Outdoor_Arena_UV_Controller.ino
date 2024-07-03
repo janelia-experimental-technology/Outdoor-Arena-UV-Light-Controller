e2ccd2a4a2b72a9067e51b7f27aa5d98524852bc
smithd3hhmi <125482553+smithd3hhmi@users.noreply.github.com>
2024-07-03 11:10:16 -0400

    Update Outdoor_Arena_UV_Controller.ino

--- Outdoor_Arena_UV_Controller/Outdoor_Arena_UV_Controller.ino
+++ Outdoor_Arena_UV_Controller/Outdoor_Arena_UV_Controller.ino
@@ -1,9 +1,12 @@
 // Outdoor Arena UV Light Controller using Seeeduino Nano and Grove DMX512 adapter
 
+// HARDWARE:
 // https://wiki.seeedstudio.com/Seeeduino-Nano/
 // https://wiki.seeedstudio.com/Grove-DMX512/
 // https://wiki.seeedstudio.com/Grove_Shield_for_Arduino_Nano/
 // https://www.amazon.com/Waterproof-Professional-Activated-Uplights-Christmas/dp/B0CL9KQQWZ?th=1
+
+// DEPENDENCIES:
 // https://github.com/PaulStoffregen/DmxSimple
 
 // Dan Smith
